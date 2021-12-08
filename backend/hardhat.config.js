5fe9aac16d6af299572a76b9b8eb9e68159cf86d
elitexpro <cryptooxelite@gmail.com>
2021-12-08 08:13:50 +0300

    ganache changed to 8545

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -38,7 +38,7 @@ module.exports = {
   solidity: "0.5.4",
   networks: {
     ganache: {
-      url: "http://127.0.0.1:7545",
+      url: "http://127.0.0.1:8545",
       gasLimit: 6000000000,
       defaultBalanceEther: 10,
     },
